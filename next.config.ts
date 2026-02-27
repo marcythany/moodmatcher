75b41feb243c5a71402f5fdc4efa5a73c2c70f2d
Marcy Thany <marcythany@gmail.com>
2026-02-27 15:17:21 -0300

    fix: nextjs setup

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,25 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'image.tmdb.org',
+				pathname: '/t/p/**', // Permite todas as imagens do TMDB
+			},
+			{
+				protocol: 'https',
+				hostname: 'static.tvmaze.com',
+				pathname: '/**', // Permite todas as imagens da TVMaze
+			},
+			{
+				protocol: 'https',
+				hostname: 'cdn.myanimelist.net',
+				pathname: '/**', // Permite todas as imagens do MyAnimeList (usado pela Jikan)
+			},
+		],
+	},
 };
 
 export default nextConfig;
